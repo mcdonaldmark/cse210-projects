a6071b77966af893106dc800340edc1ae1c090c3
mcdonaldmark <markamcdonald7@gmail.com>
2025-05-06 14:51:19 -0600

    Exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection.Emit;
 
 class Program
 {
@@ -12,9 +13,7 @@ class Program
 
         int number = -1;
         while (number != 0)
-
         {
-
             Console.Write("Enter number: ");
             string number1 = Console.ReadLine();
             number = int.Parse(number1);
@@ -24,5 +23,42 @@ class Program
                 numbers.Add(number);
             }
         }
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        Console.WriteLine($"The sum is {sum}.");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {average}.");
+
+        int max = numbers[0];
+
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
+        int min = numbers.Where(num => num > 0).Min();
+
+        Console.WriteLine($"The minimum positive value is: {min}");
+
+        numbers.Sort();
+
+        Console.WriteLine("Sorted numbers:");
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            int num = numbers[i];
+            Console.WriteLine(num);
+        }
+
+
     }
 }
