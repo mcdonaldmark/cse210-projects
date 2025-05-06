1a19d9eb125b45f3a52f959f721567087949e073
mcdonaldmark <markamcdonald7@gmail.com>
2025-05-06 02:29:37 -0600

    Exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+        while (number != 0)
+
+        {
+
+            Console.Write("Enter number: ");
+            string number1 = Console.ReadLine();
+            number = int.Parse(number1);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
     }
 }
