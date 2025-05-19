54c98c5c23d8cf028439ed4a6c086b9f0eaed334
mcdonaldmark <markamcdonald7@gmail.com>
2025-05-18 20:37:40 -0600

    sync

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int number)
+    {
+        _top = number;
+        _bottom = 1;
+    }
+
+    public Fraction(int num, int den)
+    {
+        _top = num;
+        _bottom = den;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+}
