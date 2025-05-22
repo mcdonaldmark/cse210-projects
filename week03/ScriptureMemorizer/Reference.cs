a9b0c35e8f3279ccc27ae24ef87a5e82bf9ef8a8
mcdonaldmark <markamcdonald7@gmail.com>
2025-05-22 14:06:56 -0600

    sync

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,8 +1,32 @@
+using System;
+
 public class Reference
 {
-    public string _book { get; set; }
-    public int _chapter { get; set; }
-    public int _verse { get; set; }
-    public int _endVerse { get; set; }
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
 
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_endVerse == 0)
+            return $"{_book} {_chapter}:{_verse}";
+        else
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+    }
 }
--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -1,5 +1,33 @@
+using System;
+
 public class Word
 {
-    public string text { get; set; }
-    public bool isHidden { get; set; }
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        return _isHidden ? "" : _text;
+    }
 }
